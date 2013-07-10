bbdba31d8bf410d22a6aca6a0e1de380cf555112
Yacine Rezgui <rezgui.y@gmail.com>
2013-07-10 20:28:53 +0200

    Add main application

---
+++ index.js
@@ -0,0 +1,47 @@
+// Module dependencies
+var express = require('express');
+var request = require('request');
+
+// Application settings
+var config  = require('./config.js');
+
+// Main function
+var main = function main(accessToken) {
+  // Initialize an express instance
+  var app = module.exports = express();
+
+  app.get('/', function index(req, res){
+
+    req.query.limit   = req.query.limit || config.param('default_results_limit');
+    req.query.offset  = req.query.offset || config.param('default_results_limit');
+
+    var url = config.param('facebook_api_endpoint') + config.param('facebook_page') + '/feed?&access_token=' + accessToken;
+
+    request(url, function (error, response, body) {
+      res.set('Content-Type', 'application/json');
+      res.send(body);
+    });
+  });
+
+  var port = config.param('express_port');
+  app.listen(port);
+  console.log('\n  listening on port ' + port + '\n');
+
+
+  if(process.env['NODE_ENV'] == 'development') {
+    var open  = require('open');
+    open('http://localhost:' + port);
+  }
+};
+
+
+var accessTokenUrl = config.param('facebook_api_endpoint') + 'oauth/access_token?client_id=' + config.param('facebook_app_id') + '&client_secret=' + config.param('facebook_app_secret') + '&grant_type=client_credentials';
+
+request(accessTokenUrl, function (error, response, body) {
+
+  if (!error && response.statusCode == 200) {
+
+    var accessToken = body.split('=')[1] || null;
+    main(accessToken);
+  }
+});
