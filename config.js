023a696b9d279b65708666c58b23e1a2029328b9
Yacine Rezgui <yrezgui@tequilarapido.com>
2013-07-10 11:03:00 +0200

    Add config file

---
+++ config.js
@@ -0,0 +1,18 @@
+var config = require('config-env').define('NODE_ENV', function (config) {
+  config.common({
+    name: '8fact-api-mania',
+    // Express port
+    express_port: 3008,
+  });
+  
+  config.config('development', {
+    // Facebook App Id
+    facebook_app_id: '123456789',
+    // Facebook App Secret
+    facebook_app_secret: 'FAKEAPPSECRET'
+    // Facebook Page Feed
+    facebook_page: '8fact'
+  });
+});
+
+module.exports = config;
