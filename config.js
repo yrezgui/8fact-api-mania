2fad881f80adc9c72e743f0d5d45846ca3b72d05
Yacine Rezgui <yrezgui@tequilarapido.com>
2013-07-10 11:36:18 +0200

    Add default results limit setting

--- config.js
+++ config.js
@@ -3,17 +3,19 @@ var config = require('config-env').define('NODE_ENV', function (config) {
     name: '8fact-api-mania',
     // Express port
     express_port: 3008,
+    // Facebook Graph API Endpoint
+    facebook_api_endpoint: 'https://graph.facebook.com/',
+    // Facebook Page Feed
+    facebook_page: '8fact',
+    // Facebook Page Feed
+    default_results_limit: 10
   });
   
   config.config('development', {
-    // Facebook Graph API Endpoint
-    facebook_api_endpoint: 'https://graph.facebook.com/'
     // Facebook App Id
     facebook_app_id: '123456789',
     // Facebook App Secret
     facebook_app_secret: 'FAKEAPPSECRET'
-    // Facebook Page Feed
-    facebook_page: '8fact'
   });
 });
 
